17b73fb6834805f50554adb2e9a66b8ac1d544c0
nobuyo <world_fantasy_2017@live.jp>
2017-05-30 15:41:04 +0900

    create race

---
+++ race/client.c
@@ -0,0 +1,20 @@
+#include "race.h"
+#include "mylib.h"
+#include "session.h"
+
+int main() {
+    int soc;
+    char hostname[HOSTNAME_LENGTH];
+
+    printf("Input server's hostname: ");
+    fgets(hostname, HOSTNAME_LENGTH, stdin);
+    chop_newline(hostname, HOSTNAME_LENGTH);
+
+    if ((soc=setup_client(hostname, PORT)) == -1) {
+        exit(1);
+    }
+
+    session_init(soc);
+
+    session_loop();
+}
